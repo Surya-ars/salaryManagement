f6c2185d4f05455d34347f4bcc28001c540f749d
Arnikaraj <arnikaraj2710@gmail.com>
2025-08-04 14:38:33 +0530

    initialized salarymanagement

---
+++ src/components/update-data/update-data.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-data',
+  imports: [],
+  templateUrl: './update-data.component.html',
+  styleUrl: './update-data.component.css'
+})
+export class UpdateDataComponent {
+
+}
---
+++ src/components/view-data/view-data.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-data',
+  imports: [],
+  templateUrl: './view-data.component.html',
+  styleUrl: './view-data.component.css'
+})
+export class ViewDataComponent {
+
+}
